7845f8b491635c00380e9358f0d800accb4e11cf
Robert Wiley <ylee@bodhilinux.com>
2025-05-18 12:57:20 -0400

    Improve legacy build script

--- build-legacy.sh
+++ build-legacy.sh
@@ -3,7 +3,8 @@
 
 printf "\n\033[1;33mWARNING:\033[0m For use with Bodhi Linux 7 only.\n\n"
 cd MokshaRadianceGrey
-sed -i 's/MokshaRadiance/Icons-Moksha-Radiance/g' default.edc
-./build.sh
-sed -i 's/Icons-Moksha-Radiance/MokshaRadiance/g' default.edc
+sed -i \
+  -e 's/item: "icon-theme"[ \t]*"[^"]*"/item: "icon-theme" "Icons-Moksha-Radiance"/' \
+  default.edc
+git restore default.edc
 cd ..
